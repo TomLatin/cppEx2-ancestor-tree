3c7438b0bb3919bfc10b84bccb3d68e76b09622b
TomLatin <tomlatin52@gmail.com>
2020-04-15 11:28:19 +0300

    add 2 test

--- Test.cpp
+++ Test.cpp
@@ -27,78 +27,190 @@ using namespace family;
  */
 
 
-Tree myTree("tom");
+static Tree myTree("tom");
 
-void buildTheTestTree()
+void buildTheTestTree(Tree myTree)
 {
     myTree.addMother("tom","tehila");
-        .addFather("tom","haviv");
-        /-----Sart the side of the mother-----
+    myTree.addFather("tom","haviv");
+        //-----Sart the side of the mother-----
 
-        .addMother("tehila","shara");
-        .addFather("tehila","yitzhac");
-        .addMother("shara","latipa");
-        .addFather("shara","zachi");
-        .addMother("latipa","yafa");
-        .addFather("latipa","itay");
-        .addMother("yafa","sandra");
-        .addFather("yafa","yakov");
-        .addMother("zachi","noa");
-        .addFather("zachi","elior");
-        .addMother("elior","bar");
-        .addFather("elior","tal");
-        .addMother("yitzhac","galit");
-        .addFather("yitzhac","oded");
-        .addMother("galit","car");
-        .addFather("galit","eli");
-        .addMother("oded","rose");
-        .addFather("oded","eliyahu");
-        .addMother("eliyahu","sarit");
-        .addFather("eliyahu","static");
+    myTree.addMother("tehila","shara");
+    myTree.addFather("tehila","yitzhac");
+    myTree.addMother("shara","latipa");
+    myTree.addFather("shara","zachi");
+    myTree.addMother("latipa","yafa");
+    myTree.addFather("latipa","itay");
+    myTree.addMother("yafa","sandra");
+    myTree.addFather("yafa","yakov");
+    myTree.addMother("zachi","noa");
+    myTree.addFather("zachi","elior");
+    myTree.addMother("elior","bar");
+    myTree.addFather("elior","tal");
+    myTree.addMother("yitzhac","galit");
+    myTree.addFather("yitzhac","oded");
+    myTree.addMother("galit","car");
+    myTree.addFather("galit","eli");
+    myTree.addMother("oded","rose");
+    myTree.addFather("oded","eliyahu");
+    myTree.addMother("eliyahu","sarit");
+    myTree.addFather("eliyahu","static");
 
-        /-----Sart the side of the father-----
-        .addMother("haviv","rachel");
-        .addFather("haviv","moshe");
-        .addMother("rachel","or");
-        .addFather("rachel","odi");
-        .addMother("or","yeal");
-        .addFather("or","joni");
-        .addMother("odi","tamar");
-        .addFather("odi","zvi");
-        .addMother("moshe","hana");
-        .addFather("moshe","david");
-        .addMother("hana","lee");
-        .addFather("hana","amos");
-        .addMother("amos","amit");
-        .addFather("amos","ram");
-        .addMother("galit","car");
-        .addFather("galit","eli");
-        .addMother("david","tali");
-        .addFather("david","lior");
+        //-----Sart the side of the father-----
+    myTree.addMother("haviv","rachel");
+    myTree.addFather("haviv","moshe");
+    myTree.addMother("rachel","or");
+    myTree.addFather("rachel","odi");
+    myTree.addMother("or","yeal");
+    myTree.addFather("or","joni");
+    myTree.addMother("odi","tamar");
+    myTree.addFather("odi","zvi");
+    myTree.addMother("moshe","hana");
+    myTree.addFather("moshe","david");
+    myTree.addMother("hana","lee");
+    myTree.addFather("hana","amos");
+    myTree.addMother("amos","amit");
+    myTree.addFather("amos","ram");
+    myTree.addMother("galit","car");
+    myTree.addFather("galit","eli");
+    myTree.addMother("david","tali");
+    myTree.addFather("david","lior");
 }
 
-TEST_CASE("add Mother") {
-    buildTheTestTree();
-    /-----side of the mother-----
+TEST_CASE("Test the function addMother and function relation") {
+    buildTheTestTree(myTree);
+
+            CHECK(myTree.relation("tom")==string("me"));
+
+    //-----side of the mother-----
 
             CHECK(myTree.relation("tehila")==string("mother"));
             CHECK(myTree.relation("shara")==string("grandmother"));
-            CHECK(myTree.relation("latipa")==string("great-grandfather"));
-            CHECK(myTree.relation("galit")==string("great-grandfather"));
-            CHECK(myTree.relation("yafa")==string("great-great-grandfather"));
-            CHECK(myTree.relation("noa")==string("great-great-grandfather"));
-            CHECK(myTree.relation("car")==string("great-great-grandfather"));
-            CHECK(myTree.relation("rose")==string("great-great-grandfather"));
-            CHECK(myTree.relation("sandra")==string("great-great-great-grandfather"));
-            CHECK(myTree.relation("bar")==string("great-great-great-grandfather"));
-            CHECK(myTree.relation("sarit")==string("great-great-great-grandfather"));
+            CHECK(myTree.relation("latipa")==string("great-grandmother"));
+            CHECK(myTree.relation("galit")==string("great-grandmother"));
+            CHECK(myTree.relation("yafa")==string("great-great-grandmother"));
+            CHECK(myTree.relation("noa")==string("great-great-grandmother"));
+            CHECK(myTree.relation("car")==string("great-great-grandmother"));
+            CHECK(myTree.relation("rose")==string("great-great-grandmother"));
+            CHECK(myTree.relation("sandra")==string("great-great-great-grandmother"));
+            CHECK(myTree.relation("bar")==string("great-great-great-grandmother"));
+            CHECK(myTree.relation("sarit")==string("great-great-great-grandmother"));
 
-    /-----side of the father-----
+            CHECK(myTree.relation("tehila")!=string("grandmother"));
+            CHECK(myTree.relation("shara")!=string("mother"));
+            CHECK(myTree.relation("latipa")!=string("great-great-grandmother"));
+            CHECK(myTree.relation("galit")!=string("great-grandmother"));
+            CHECK(myTree.relation("yafa")!=string("great-grandmother"));
+            CHECK(myTree.relation("noa")!=string("great-grandmother"));
+            CHECK(myTree.relation("car")!=string("great-grandmother"));
+            CHECK(myTree.relation("rose")!=string("great-grandmother"));
+            CHECK(myTree.relation("sandra")!=string("great-great-great-great-great-grandmother"));
+            CHECK(myTree.relation("bar")!=string("great-great-great-great-great-grandmother"));
+            CHECK(myTree.relation("sarit")!=string("great-grandmother"));
+
+    //-----side of the father-----
 
             CHECK(myTree.relation("rachel")==string("grandmother"));
-            CHECK(myTree.relation("or")==string("great-grandfather"));
-            CHECK(myTree.relation("yeal")==string("great-great-grandfather"));
-            CHECK(myTree.relation("tehila")!=string("grandmother"));
+            CHECK(myTree.relation("or")==string("great-grandmother"));
+            CHECK(myTree.relation("hana")==string("great-grandmother"));
+            CHECK(myTree.relation("yeal")==string("great-great-grandmother"));
+            CHECK(myTree.relation("tamar")==string("great-great-grandmother"));
+            CHECK(myTree.relation("lee")==string("great-great-grandmother"));
+            CHECK(myTree.relation("tali")==string("great-great-grandmother"));
+            CHECK(myTree.relation("amit")==string("great-great-great-grandmother"));
 
+            CHECK(myTree.relation("rachel")!=string("great-great-grandmother"));
+            CHECK(myTree.relation("or")!=string("grandmother"));
+            CHECK(myTree.relation("hana")!=string("great-great-great-grandmother"));
+            CHECK(myTree.relation("yeal")!=string("great-grandmother"));
+            CHECK(myTree.relation("tamar")!=string("great-great-great-great-grandmother"));
+            CHECK(myTree.relation("lee")!=string("mother"));
+            CHECK(myTree.relation("tali")!=string("great-grandmother"));
+            CHECK(myTree.relation("amit")!=string("great-great-great-great-grandmother"));
 
+    //-----NOT in the family-----
+
+            CHECK(myTree.relation("x")==string("unrelated"));
+            CHECK(myTree.relation("orian")==string("unrelated"));
+            CHECK(myTree.relation("yirat")==string("unrelated"));
+            CHECK(myTree.relation("gargamel")==string("unrelated"));
+            CHECK(myTree.relation("dardas")==string("unrelated"));
+            CHECK(myTree.relation("shani")==string("unrelated"));
+            CHECK(myTree.relation("orel")==string("unrelated"));
+            CHECK(myTree.relation("haivva")==string("unrelated"));
+            CHECK(myTree.relation("sandtra")==string("unrelated"));
+            CHECK(myTree.relation("baur")==string("unrelated"));
+            CHECK(myTree.relation("sarrit")==string("unrelated"));
+            CHECK(myTree.relation("til")==string("unrelated"));
+            CHECK(myTree.relation("mama")==string("unrelated"));
+            CHECK(myTree.relation("papa")==string("unrelated"));
+}
+
+TEST_CASE("Test the function addFather and function relation") {
+    buildTheTestTree(myTree);
+
+            CHECK(myTree.relation("tom")==string("me"));
+
+    //-----side of the mother-----
+
+            CHECK(myTree.relation("haviv")==string("father"));
+            CHECK(myTree.relation("yitzhac")==string("grandfather"));
+            CHECK(myTree.relation("zachi")==string("great-grandfather"));
+            CHECK(myTree.relation("oded")==string("great-grandfather"));
+            CHECK(myTree.relation("itay")==string("great-great-grandfather"));
+            CHECK(myTree.relation("elior")==string("great-great-grandfather"));
+            CHECK(myTree.relation("eli")==string("great-great-grandfather"));
+            CHECK(myTree.relation("eliyahu")==string("great-great-grandfather"));
+            CHECK(myTree.relation("yakov")==string("great-great-great-grandfather"));
+            CHECK(myTree.relation("tal")==string("great-great-great-grandfather"));
+            CHECK(myTree.relation("static")==string("great-great-great-grandfather"));
+
+            CHECK(myTree.relation("haviv")!=string("grandfather"));
+            CHECK(myTree.relation("yitzhac")!=string("great-grandfather"));
+            CHECK(myTree.relation("zachi")!=string("grandfather"));
+            CHECK(myTree.relation("oded")!=string("great-great-great-grandfather"));
+            CHECK(myTree.relation("itay")!=string("great-great-great-great-great-grandfather"));
+            CHECK(myTree.relation("elior")!=string("great-grandfather"));
+            CHECK(myTree.relation("eli")!=string("great-great-great-grandfather"));
+            CHECK(myTree.relation("eliyahu")!=string("great-great-great-great-grandfather"));
+            CHECK(myTree.relation("yakov")!=string("grandfather"));
+            CHECK(myTree.relation("tal")!=string("great-great-great-great-grandfather"));
+            CHECK(myTree.relation("static")!=string("great-grandfather"));
+
+    //-----side of the father-----
+
+            CHECK(myTree.relation("moshe")==string("grandfather"));
+            CHECK(myTree.relation("odi")==string("great-grandfather"));
+            CHECK(myTree.relation("david")==string("great-grandfather"));
+            CHECK(myTree.relation("joni")==string("great-great-grandfather"));
+            CHECK(myTree.relation("zvi")==string("great-great-grandfather"));
+            CHECK(myTree.relation("amos")==string("great-great-grandfather"));
+            CHECK(myTree.relation("lior")==string("great-great-grandfather"));
+            CHECK(myTree.relation("ram")==string("great-great-great-grandfather"));
+
+            CHECK(myTree.relation("moshe")!=string("great-grandfather"));
+            CHECK(myTree.relation("odi")!=string("grandfather"));
+            CHECK(myTree.relation("david")!=string("great-great-grandfather"));
+            CHECK(myTree.relation("joni")!=string("great-grandfather"));
+            CHECK(myTree.relation("zvi")!=string("great-great-great-grandfather"));
+            CHECK(myTree.relation("amos")!=string("great-great-great-great-grandfather"));
+            CHECK(myTree.relation("lior")!=string("great-great-great-great-great-grandfather"));
+            CHECK(myTree.relation("ram")!=string("father"));
+
+    //-----NOT in the family-----
+
+            CHECK(myTree.relation("y")==string("unrelated"));
+            CHECK(myTree.relation("gil")==string("unrelated"));
+            CHECK(myTree.relation("mayan")==string("unrelated"));
+            CHECK(myTree.relation("inbal")==string("unrelated"));
+            CHECK(myTree.relation("adin")==string("unrelated"));
+            CHECK(myTree.relation("adir")==string("unrelated"));
+            CHECK(myTree.relation("yair")==string("unrelated"));
+            CHECK(myTree.relation("miri")==string("unrelated"));
+            CHECK(myTree.relation("norit")==string("unrelated"));
+            CHECK(myTree.relation("efrat")==string("unrelated"));
+            CHECK(myTree.relation("tikva")==string("unrelated"));
+            CHECK(myTree.relation("avital")==string("unrelated"));
+            CHECK(myTree.relation("eran")==string("unrelated"));
+            CHECK(myTree.relation("gilad")==string("unrelated"));
 }
