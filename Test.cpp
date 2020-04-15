48b07381c504759c87a429d0ad7f1cf8ec31a10a
TomLatin <tomlatin52@gmail.com>
2020-04-15 13:40:50 +0300

    done with the tests

--- Test.cpp
+++ Test.cpp
@@ -214,3 +214,77 @@ TEST_CASE("Test the function addFather and function relation") {
             CHECK(myTree.relation("eran")==string("unrelated"));
             CHECK(myTree.relation("gilad")==string("unrelated"));
 }
+
+TEST_CASE("Test the function find") {
+    buildTheTestTree(myTree);
+
+            CHECK(myTree.find("me")==string("tom"));
+
+            CHECK(myTree.find("mother")==string("tehila"));
+            CHECK(myTree.find("grandmother")==string("shara") || myTree.find("grandmother")==string("rachel" ));
+            CHECK(myTree.find("great-grandmother")==string("latipa") || (myTree.find("great-grandmother")==string("galit")
+                 (myTree.find("great-grandmother")==string("or") || (myTree.find("great-grandmother")==string("hana") );
+            CHECK(myTree.find("great-great-grandmother")==string("yafa") || myTree.find("great-great-grandmother")==string("noa")
+                 myTree.find("great-great-grandmother")==string("car") || myTree.find("great-great-grandmother")==string("rose")
+                 myTree.find("great-great-grandmother")==string("yeal") || myTree.find("great-great-grandmother")==string("tamar")
+                 myTree.find("great-great-grandmother")==string("lee") || myTree.find("great-great-grandmother")==string("tali"));
+            CHECK(myTree.find("great-great-great-grandmother")==string("sandra") || myTree.find("great-great-great-grandmother")==string("bar")
+                  myTree.find("great-great-great-grandmother")==string("sarit") || myTree.find("great-great-great-grandmother")==string("amit"));
+
+            CHECK(myTree.find("father")==string("haviv"));
+            CHECK(myTree.find("grandfather")==string("yitzhac") || myTree.find("grandfather")==string("moshe" ));
+            CHECK(myTree.find("great-grandfather")==string("zachi") || (myTree.find("great-grandfather")==string("oded")
+                 (myTree.find("great-grandfather")==string("odi") || (myTree.find("great-grandfather")==string("david") );
+            CHECK(myTree.find("great-great-grandfather")==string("itay") || myTree.find("great-great-grandfather")==string("elior")
+                  myTree.find("great-great-grandfather")==string("eli") || myTree.find("great-great-grandfather")==string("eliyahu")
+                  myTree.find("great-great-grandfather")==string("joni") || myTree.find("great-great-grandfather")==string("zvi")
+                  myTree.find("great-great-grandfather")==string("amos") || myTree.find("great-great-grandfather")==string("lior"));
+            CHECK(myTree.find("great-great-great-grandfather")==string("yakov") || myTree.find("great-great-great-grandfather")==string("tal")
+                  myTree.find("great-great-great-grandfather")==string("static") || myTree.find("great-great-great-grandfather")==string("ram"));
+
+            //Should throw an error
+             CHECK_THROWS(myTree.find("nephew"));
+             CHECK_THROWS(myTree.find("Niece"));
+             CHECK_THROWS(myTree.find("Mother-in-law"));
+             CHECK_THROWS(myTree.find("son-in-law"));
+             CHECK_THROWS(myTree.find("Uncle"));
+             CHECK_THROWS(myTree.find("aunt"));
+             CHECK_THROWS(myTree.find("Cousin"));
+             CHECK_THROWS(myTree.find("son"));
+             CHECK_THROWS(myTree.find("daughter"));
+}
+
+TEST_CASE("Test the function remove") {
+            buildTheTestTree(myTree);
+
+            myTree.remove("oded");
+
+            CHECK(myTree.relation("oded")==string("unrelated"));
+            CHECK(myTree.relation("rose")==string("unrelated"));
+            CHECK(myTree.relation("eliyahu")==string("unrelated"));
+            CHECK(myTree.relation("sarit")==string("unrelated"));
+            CHECK(myTree.relation("static")==string("unrelated"));
+
+            myTree.remove("shara");
+
+            CHECK(myTree.relation("shara")==string("unrelated"));
+            CHECK(myTree.relation("latipa")==string("unrelated"));
+            CHECK(myTree.relation("eliyahu")==string("unrelated"));
+            CHECK(myTree.relation("zachi")==string("unrelated"));
+            CHECK(myTree.relation("noa")==string("unrelated"));
+            CHECK(myTree.relation("elior")==string("unrelated"));
+            CHECK(myTree.relation("bar")==string("unrelated"));
+            CHECK(myTree.relation("tal")==string("unrelated"));
+            CHECK(myTree.relation("itay")==string("unrelated"));
+            CHECK(myTree.relation("yafa")==string("unrelated"));
+            CHECK(myTree.relation("sandra")==string("unrelated"));
+            CHECK(myTree.relation("yakov")==string("unrelated"));
+
+            myTree.remove("amit");
+
+            CHECK(myTree.relation("amos")==string("great-great-grandfather"));
+            CHECK(myTree.relation("ram")==string("great-great-great-grandfather"));
+
+            //Should throw an error
+            CHECK_THROWS(myTree.remove("tom"));
+}
